5b84d84db82e31b1271ce159460deac096b70515
Eragabu <eragabu@gmail.comgit config --global user.email eragabu@gmail.com>
2022-07-06 06:15:50 -0700

    First

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,7 +4,6 @@ include "main.h"
  * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
-
 int main(void)
 {
 	int i;
@@ -14,3 +13,4 @@ int main(void)
 
 	return (0);
 }
+
